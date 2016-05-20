6379821141570393f086de4bc52c204edf294200
vladislav.borisov <vladislav.borisov@WSBPSOF199.bpsf.bulpros.com>
2016-05-20 11:32:43 +0300

    printing Hello OSGi console every 5 sec

--- src/com/sherybedrock/sample/helloworld/Activator.java
+++ src/com/sherybedrock/sample/helloworld/Activator.java
@@ -2,27 +2,20 @@ package com.sherybedrock.sample.helloworld;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Activator implements BundleActivator {
-	private Logger logger=LoggerFactory.getLogger(this.getClass());
+	private MyThread myThread;
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
-	 */
 	public void start(BundleContext context) throws Exception {
-		System.out.println("Hello World!!");
-		logger.error("logging error {}", this);
+		System.out.println("Starting com.sherybedrock.sample.helloworld");
+		myThread = new MyThread();
+		myThread.start();
 	}
-	
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
+
 	public void stop(BundleContext context) throws Exception {
-		System.out.println("Goodbye Worldaaa!!");
+		System.out.println("Stopping com.sherybedrock.sample.helloworld");
+		myThread.stopThread();
+		myThread.join();
 	}
 
 }
